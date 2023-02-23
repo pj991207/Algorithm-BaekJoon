6e69c66252e7fde76cc701f78ec3b5c84192b0c0
Lion_King <pj991207@gmail.com>
2023-02-23 11:44:56 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,6 +4,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    /***
+     * O/X 선택으로 N과 M (2) 풀기 시간복잡도는 O(2^n)으로 O(n!)보다 빠르다
+     * 여기서 주의할 점은 정답도 오른차순으로 출력되기 때문에 ans[index]=value;가 먼저와야한다.
+     */
     static int[] ans = new int[9];
 
     public static void main(String[] args) throws IOException {
@@ -14,7 +18,7 @@ public class Main {
         go(0, 1, N, M);
     }
 
-    public static void go(int index, int start, int n, int m) {
+    public static void go(int index, int value, int n, int m) {
         if (index == m) {
             StringBuilder sb = new StringBuilder();
             for (int i : ans) {
@@ -27,9 +31,16 @@ public class Main {
             System.out.println(sb);
             return;
         }
-        for (int i = start; i <= n; i++) {
-            ans[index] = i;
-            go(index + 1, ans[index] + 1, n, m);
-        }
+        if (value > n) return;
+        /***
+         * O인 경우
+         */
+        ans[index] = value;
+        go(index + 1, value + 1, n, m);
+        /***
+         * X인 경우
+         */
+        ans[index] = 0;
+        go(index, value + 1, n, m);
     }
 }
