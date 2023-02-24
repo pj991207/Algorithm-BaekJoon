313e56b908701113f90cc8dc345d703f1dc8b93a
Lion_King <pj991207@gmail.com>
2023-02-24 19:32:33 +0900

    [Silver III] Title: N과 M (7), Time: 624 ms, Memory: 123840 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] ans = new int[8];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        go(0, N, M, arr, sb);
+        System.out.println(sb);
+    }
+
+    public static void go(int index, int n, int m, int[] arr, StringBuilder sb) {
+        if (index == m) {
+            for (int i : ans) {
+                if (i == 0) {
+                    break;
+                }
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            ans[index] = arr[i];
+            go(index + 1, n, m, arr, sb);
+        }
+    }
+}
