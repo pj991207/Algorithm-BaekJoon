0c3c7cc54d835166d75cd2f4a3f6907f9d61afb9
병제박 <pj991207@gmail.com>
2023-04-04 11:33:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 13.38 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        String ans = "";
+        while(n != 0) {
+            ans += n%3;
+            n = n/3;
+        }
+        return Integer.parseInt(ans,3);
+    }
+}
