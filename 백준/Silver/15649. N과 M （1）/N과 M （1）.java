981d519a1e9221808010d5f1e0e6532180b2fa16
ISO-pj991207 <pj991207@gmail.com>
2023-08-01 16:11:08 +0900

    [Silver III] Title: N과 M (1), Time: 332 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,37 +1,39 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-    public static int N;
-    public static int M;
-    public static int[] arr;
-    public static boolean[] visit;
-    public static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[M];
-        visit = new boolean[N];
-        dfs(0);
-        System.out.println(sb.substring(0, sb.length() - 1));
-    }
-
-    public static void dfs(int perm) {
-        if (perm == M) {
-            for (int i : arr) {
-                sb.append(i + " ");
-            }
-            sb.append("\n");
-        } else {
-            for (int i = 0; i < N; i++) {
-                if (!visit[i]) {
-                    visit[i] = true;
-                    arr[perm] = i + 1;
-                    dfs(perm + 1);
-                    visit[i] = false;
-                }
-            }
-        }
-    }
+	static int N;
+	static int R;
+	static int[] p;
+	static int[] nums;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		N = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		p = new int[N];
+		visited = new boolean[N];
+		nums = new int[R];
+		Arrays.setAll(p, i ->i+1);
+		Perm(0);
+		System.out.println(sb);
+	}
+	static void Perm(int c) {
+		if(c==R) {
+			for(int i =0; i<R;i++) {
+				sb.append(nums[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i = 0; i<N;i++) {
+			if(visited[i])continue;
+			visited[i]=true;
+			nums[c]=p[i];
+			Perm(c+1);
+			visited[i]=false;
+		}
+	}
+	
 }
