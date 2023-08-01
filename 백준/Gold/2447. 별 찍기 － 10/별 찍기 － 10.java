96d0e81e6c5ae7299044a5c66f27034b99613283
ISO-pj991207 <pj991207@gmail.com>
2023-08-01 14:30:28 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 368 ms, Memory: 58912 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,44 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 public class Main {
+	static int[][] arr;
 	static int N;
-	static int[][] star;
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(bf.readLine());
+		arr = new int[N][N];
 		StringBuilder sb = new StringBuilder();
-		
-		star = new int[N][N];
 		star(0,0,N);
-		
-		for(int i = 0; i<N;i++) {
+		for(int i=0 ; i<N;i++) {
 			for(int j = 0; j<N;j++) {
-				sb.append(star[i][j]==1?"*":" ");
+				if(arr[i][j]==1)sb.append("*");
+				if(arr[i][j]==0)sb.append(" ");
 			}
 			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
-	static void star(int r, int c, int n) {
-		
-		//end comdaitons basecondition
+	static void star(int a, int b, int n) {
 		if(n==3) {
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					if(i==1 && j==1)continue;// 다음 스탭 -> 중앙일때 아무일다안함
-					star[r+i][c+j]=1;
+			for(int i = 0; i<3;i++) {
+				for(int j = 0;j<3;j++) {
+					if(i==1&&j==1)continue;
+					arr[a+i][b+j] = 1;
 				}
 			}
 		}
 		else {
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					if(i==1 && j==1)continue;// 다음 스탭 -> 중앙일때 아무일다안함
-					star(r+i*n/3,c+j*n/3,n/3);
+			for(int i =0;i<3;i++) {
+				for(int j = 0;j<3;j++) {
+					if(i==1&&j==1)continue;
+					star(a+i*n/3,b+j*n/3,n/3);
 				}
 			}
 		}
-		
 	}
 }
