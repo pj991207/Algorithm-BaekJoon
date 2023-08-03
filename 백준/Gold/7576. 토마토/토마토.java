a5eec22d39fc5e7d01860d60e90feb318719d9c2
ISO-pj991207 <pj991207@gmail.com>
2023-08-03 20:56:53 +0900

    [Gold V] Title: 토마토, Time: 636 ms, Memory: 119996 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,102 +1,67 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-	static int[][] box;
-	static int M;
 	static int N;
-	static int[][] value;
-	//static boolean[][] check;
-	static int[] dy = {-1,0,1,0};
-	static int[] dx = {0,1,0,-1};
-	public static void main(String[] args) throws IOException {
+	static int M;
+	static int[][] box;
+	static int[] dy = { 1, 0, -1, 0 };
+	static int[] dx = { 0, 1, 0, -1 };
+	static int day = 0;
+	static Queue<int[]> q = new LinkedList<>();
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		// 초기화
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		box = new int[N][M];
-		value = new int[N][M];
-		for(int i = 0;i<N;i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(bf.readLine());
-			for(int j = 0; j<M;j++) {
-				box[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-		for(int i =0;i<N;i++) {
-			for(int j =0;j<M;j++) {
-				if(box[i][j]==0) {
-					value[i][j] = -1;
-				} else {
-					value[i][j] = -2;
+			for (int j = 0; j < M; j++) {
+				box[i][j] = Integer.parseInt(st.nextToken());
+				if(box[i][j]==1){
+					q.add(new int[]{i,j});
 				}
 			}
 		}
-		Queue<Pair> x = new LinkedList<>();
-		boolean flag = false;
-		for(int i = 0;i<N;i++) {
-			for(int j = 0; j<M;j++) {
-				if(box[i][j]==1) {
-					Pair pair = new Pair(i,j);
-					x.add(pair);
-				}
-				if(box[i][j]==0)
-					flag = true;
-					continue;
-				}
-		}
-		int answer = 0;
-		if(!flag) {
-			System.out.println(answer);
-		}
-		else {
-			answer=bfs(x);
-			for(int i = 0;i<N;i++) {
-				for(int j = 0;j<M;j++) {
-					if(value[i][j]==-1) {
-						answer = -1;
-						System.out.print(answer);
-						return;
-					}
-				}
-			}
-			System.out.print(answer);
-		}
+		//탐색
+		bfs();
+		//확인
+		check();
+		
 	}
-	static int bfs(Queue<Pair> p) {
-		Queue<Pair> q = new LinkedList<>();
-		int len = p.size();
-		int answer = 0;
-		for(int i = 0; i<len;i++) {
-			Pair pair = p.poll();
-			value[pair.y][pair.x]=0;
-			q.add(pair);
-		}
+	//bfs를 통해서 익은 토마토를 탐색합니다.
+	static void bfs() {
 		while(!q.isEmpty()) {
-			Pair pair = q.poll();
-			int x = pair.x;
-			int y = pair.y;
-			for(int i=0;i<4;i++) {
-				int c= y + dy[i];
-				int r = x + dx[i];
-				if(c>=0&&c<N&&r>=0&&r<M) {
-					if(value[c][r]==-1)
-					{
-						value[c][r] = value[y][x]+1;
-						if(answer<value[c][r]) {
-							answer = value[c][r];
-						}
-						q.add(new Pair(c,r));
-					}
+			int[] pos = q.poll();
+			for(int i = 0; i<4;i++) {
+				int y = pos[0]+dy[i];
+				int x = pos[1]+dx[i];
+				if(x>=0&&x<M&&y>=0&&y<N&&box[y][x]==0) {
+					box[y][x]=box[pos[0]][pos[1]]+1;
+					day = box[y][x];
+					q.add(new int[] {y,x});
 				}
 			}
 		}
-		return answer;
 	}
-}
-class Pair{
-	int x;
-	int y;
-	Pair(int y,int x){
-		this.x = x;
-		this.y = y;
+	//안익은 토마토 확인 및 날짜 출력
+	static void check() {
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				if(box[i][j]==0) {
+					System.out.println(-1);
+					return;
+				}
+			}
+		}
+		if(day==0) {
+			System.out.println(0);
+		}else {
+			System.out.println(day-1);
+		}
 	}
 }
