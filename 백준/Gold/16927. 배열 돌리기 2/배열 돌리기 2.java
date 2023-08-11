be34d8915b0b6a3f92776e9d09f5e3b5e7d7581c
ISO-pj991207 <pj991207@gmail.com>
2023-08-11 14:49:05 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 1480 ms, Memory: 38036 KB -BaekjoonHub

---
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N;
+	static int M;
+	static int R;
+	static int[][] arr;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		rotate(0, 0, N, M);
+		for(int i = 0; i< N;i++) {
+			for(int j = 0;j<M;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println("");
+		}
+	}
+
+	static void rotate(int y, int x, int n, int m) {
+		if (n == 0 || m == 0) {
+			return;
+		}
+		int T = R%(2*m+2*n-4);
+		for (int i = 0; i < T; i++) {
+			int before = arr[y][x];
+			int temp = 0;
+			for (int j = 1; j < n; j++) {
+				temp = arr[y + j][x];
+				arr[y + j][x] = before;
+				before = temp;
+			}
+			for (int j = 1; j < m; j++) {
+				temp = arr[n - 1+y][x + j];
+				arr[n - 1 + y][x + j] = before;
+				before = temp;
+			}
+			for (int j = 1; j < n; j++) {
+				temp = arr[n - 1 - j+y][m - 1+x];
+				arr[n - 1 - j + y][m - 1 + x] = before;
+				before = temp;
+			}
+			for (int j = 1; j < m; j++) {
+				temp = arr[y][m - 1 - j + x];
+				arr[y][m - 1 - j + x] = before;
+				before = temp;
+			}
+		}
+		rotate(y + 1, x + 1, n - 2, m - 2);
+	}
+}
