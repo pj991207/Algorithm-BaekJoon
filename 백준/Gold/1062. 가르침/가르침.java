84c680b826d31202fbe0146e938b7ff1959377ec
ISO-pj991207 <pj991207@gmail.com>
2023-08-14 23:08:43 +0900

    [Gold IV] Title: 가르침, Time: 388 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -2,77 +2,72 @@ import java.util.*;
 import java.io.*;
 public class Main {
 	static int N;
-	static int M;
-	static boolean[][] arr;
-	static boolean[] dict;
-	static int[] nums;
-	static int MAX;
-	static int valid;
+	static int K;
+	static int R;
+	static int MAX = 0;
+	static boolean[] word = new boolean[26];
+	static ArrayList<boolean[]> wordlist = new ArrayList<>();
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
+		word['a'-'a']=true;
+		word['n'-'a']=true;
+		word['t'-'a']=true;
+		word['i'-'a']=true;
+		word['c'-'a']=true;
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		if(M<5) {
+		K = Integer.parseInt(st.nextToken());
+		R = K-5;
+		if(R<0) {
 			System.out.println(0);
-			System.exit(0);
+			return;
 		}
-		
-		MAX = 0;
-		dict = new boolean[26];
-		arr = new boolean[N][26];
-		nums = new int[N];
-		
-		M = M - 5;
-		dict['a'-'a'] = true;
-		dict['n'-'a'] = true;
-		dict['t'-'a'] = true;
-		dict['i'-'a'] = true;
-		dict['c'-'a'] = true;
 		for(int i = 0; i<N;i++) {
-			char[] charArr = bf.readLine().toCharArray();
-			int cnt = 0;
-			for(int j = 0; j<charArr.length;j++) {
-				if(arr[i][charArr[j]-'a']==true) {
-					continue;
+			boolean[] temp = new boolean[26];
+			temp['a'-'a']=true;
+			temp['n'-'a']=true;
+			temp['t'-'a']=true;
+			temp['i'-'a']=true;
+			temp['c'-'a']=true;
+			String s = bf.readLine();
+			s = s.substring(4,s.length()-4);
+			char[] arr = s.toCharArray();
+			for(int j=0;j<arr.length;j++) {
+				if(!temp[arr[j]-'a']) {
+					temp[arr[j]-'a']=true;
 				}
-				arr[i][charArr[j]-'a'] = true;
-				cnt++;
 			}
-			nums[i] = cnt-5;
+			wordlist.add(temp);
 		}
 		dfs(0,0);
 		System.out.println(MAX);
 	}
-	static void dfs(int cnt,int start) {
-		if(cnt==M) {
-			valid = 0;
-			for(int i = 0; i<N;i++) {
+	static void dfs(int x,int start) {
+		if(x==R) {
+			int cnt = 0;
+			for(boolean[] a : wordlist) {
 				boolean flag = true;
-				if(nums[i]>M) {
-					continue;
-				}
-				for(int j = 0; j<26;j++) {
-					if(arr[i][j]==true) {
-						if(dict[j]!=true) {
+				for(int i = 0; i<26;i++) {
+					if(a[i]) {
+						if(!word[i]) {
 							flag = false;
 							break;
 						}
 					}
 				}
 				if(flag) {
-					valid++;
+					cnt++;
 				}
 			}
-			MAX = Math.max(MAX,valid);
+			MAX = Math.max(MAX,cnt);
 			return;
 		}
-		for(int i = start; i<26;i++) {
-			if(dict[i])continue;
-			dict[i] = true;
-			dfs(cnt+1,i+1);
-			dict[i] = false;
+		for(int i = start;i<26;i++) {
+			if(word[i])continue;
+			word[i]=true;
+			dfs(x+1,i+1);
+			word[i]=false;
 		}
 	}
 }
