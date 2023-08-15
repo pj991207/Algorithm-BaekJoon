753f5c3a1a8c349d306267a8a0d55a44f7aa5867
ISO-pj991207 <pj991207@gmail.com>
2023-08-16 08:15:57 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -2,31 +2,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
 	static int N;
 	static int r;
 	static int c;
-	static int sum;
-	static int[][] map;
+	static int sum=0;
+	
 	public static void main(String[] args) throws Exception {
+	
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		N = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
-		N = (int)Math.pow(2,N);
-		Z(0,0,N);
+		Z(0,0,1<<N);
 	}
-	static void Z(int sr,int sc,int width) {
-		if(sr==r&&sc==c) {
+	
+	static void Z(int y,int x, int size) {
+		if(y==r&&x==c) {
 			System.out.println(sum);
 			return;
 		}
-		if(r>=sr&&r<sr+width&&c>=sc&&c<sc+width) {
-			width = width/2;
-			Z(sr,sc,width);
-			Z(sr,sc+width,width);
-			Z(sr+width,sc,width);
-			Z(sr+width,sc+width,width);
-		}else sum+=width*width;
+		if(y<=r&&r<y+size&&x<=c&&c<x+size) {
+			Z(y,x,size/2);
+			Z(y,x+size/2,size/2);
+			Z(y+size/2,x,size/2);
+			Z(y+size/2,x+size/2,size/2);
+		}else {
+			sum += size*size;
+		}
 	}
 }
