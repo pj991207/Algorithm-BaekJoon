b8b08d135b048dc35eb9d27f62ea5b5cd8e162b6
ISO-pj991207 <pj991207@gmail.com>
2023-08-18 11:18:26 +0900

    [Silver I] Title: 부등호, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -7,67 +7,61 @@ public class Main {
 	static long MIN;
 	static String minStr;
 	static int N;
-	//static int[] p = {0,1,2,3,4,5,6,7,8,9};
+	static int[] p;
 	static int[] nums;
 	static boolean[] visited;
 	static String[] arr;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(bf.readLine());
 		arr = new String[N];
-		visited = new boolean[10];
-		nums = new int[N+1];
 		MAX = 0;
 		MIN = (long)Math.pow(10,N+1);
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		for(int i = 0; i<N;i++) {
 			arr[i] = st.nextToken();
 		}
+		
+		visited = new boolean[10];
+		p = new int[] {9,8,7,6,5,4,3,2,1,0};
+		nums = new int[N+1];
 		Perm(0);
-		System.out.println(maxStr);
-		System.out.println(minStr);
+		visited = new boolean[10];
+		p = new int[] {0,1,2,3,4,5,6,7,8,9};
+		Perm(0);
+		
+		System.out.println(sb);
 	}
-	static void Perm(int cnt) {
+	
+	static boolean Perm(int cnt) {
 		if(cnt==N+1) {
-			for(int i = 0; i<N;i++) {
-				String check = arr[i];
-				int a = nums[i];
-				int b = nums[i+1];
-				if(check.equals("<")) {
-					if(a>b) {
-						return;
-					}
-				}
-				if(check.equals(">")) {
-					if(a<b) {
-						return;
-					}
-				}
+			for(int i : nums) {
+				sb.append(i);
 			}
-			
-			String str = "";
-			for(int i = 0;i<N+1;i++) {
-				str += String.valueOf(nums[i]);
-			}
-			long ans = Long.parseLong(str);
-			
-			if(MAX<ans) {
-				maxStr = str;
-				MAX = ans;
-			}
-			if(MIN>ans) {
-				minStr= str;
-				MIN = ans;
-			}
-			return;
+			sb.append('\n');
+			return true;
 		}
 		for(int i = 0 ; i<10;i++) {
 			if(visited[i])continue;
 			visited[i] = true;
-			nums[cnt]=i;
-			Perm(cnt+1);
+			nums[cnt]=p[i];
+			if(cnt==0||compare(cnt)) {
+				if(Perm(cnt+1)) return true;
+			}
 			visited[i] = false;
 		}
+		return false;
+	}
+	
+	static boolean compare(int i){
+		if(arr[i-1].equals("<")) {
+			return nums[i-1]<nums[i];
+		}
+		if(arr[i-1].equals(">")) {
+			return nums[i-1]>nums[i];
+		}
+		return true;
 	}
 }
