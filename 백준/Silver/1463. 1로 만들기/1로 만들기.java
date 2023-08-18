4babadf230200c73b55127c8af99248a2eb19eda
ISO-pj991207 <pj991207@gmail.com>
2023-08-18 13:23:32 +0900

    [Silver III] Title: 1로 만들기, Time: 272 ms, Memory: 135136 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(bf.readLine());
-        int[] memo = new int[1000001];
-        for (int i = 2; i <= num; i++) {
-            memo[i] = memo[i - 1] + 1;
-            if (i % 2 == 0 && memo[i] > memo[i / 2] + 1) {
-                memo[i] = memo[i / 2] + 1;
-            }
-            if (i % 3 == 0 && memo[i] > memo[i / 3] + 1) {
-                memo[i] = memo[i / 3] + 1;
-            }
-        }
-        System.out.println(memo[num]);
-    }
+	static int N;
+	static int MIN = 1000000;
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(bf.readLine());
+		Queue<Box> q = new ArrayDeque<>();
+		q.add(new Box(N,0));
+		while(!q.isEmpty()) {
+			Box box = q.poll();
+			int num = box.num;
+			int cnt = box.cnt;
+			if(num == 1) {
+				MIN = Math.min(MIN,cnt);
+				break;
+			}
+			if(cnt>MIN) {
+				continue;
+			}
+			if(num%3==0) {
+				q.add(new Box(num/3,cnt+1));
+			}
+			if(num%2==0) {
+				q.add(new Box(num/2,cnt+1));
+			}
+			q.add(new Box(num-1,cnt+1));
+		}
+		System.out.println(MIN);
+	}
+	static class Box{
+		int num;
+		int cnt;
+		Box(int num,int cnt){
+			this.num = num;
+			this.cnt = cnt;
+		}
+	}
 }
