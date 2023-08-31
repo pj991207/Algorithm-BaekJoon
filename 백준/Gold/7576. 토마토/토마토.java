27e5c9542858bfe3c1f2bd95f5e145fef3b26b6e
ISO-pj991207 <pj991207@gmail.com>
2023-08-31 23:54:58 +0900

    [Gold V] Title: 토마토, Time: 672 ms, Memory: 103836 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -4,64 +4,63 @@ import java.io.*;
 public class Main {
 	static int N;
 	static int M;
-	static int[][] box;
-	static int[] dy = { 1, 0, -1, 0 };
+	static int[][] map;
+	static ArrayDeque<Node> deq;
+	static int[] dy = { -1, 0, 1, 0 };
 	static int[] dx = { 0, 1, 0, -1 };
-	static int day = 0;
-	static Queue<int[]> q = new LinkedList<>();
+	static int cnt = 0;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-
-		// 초기화
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		box = new int[N][M];
+		map = new int[N][M];
+		deq = new ArrayDeque<Node>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(bf.readLine());
 			for (int j = 0; j < M; j++) {
-				box[i][j] = Integer.parseInt(st.nextToken());
-				if(box[i][j]==1){
-					q.add(new int[]{i,j});
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) {
+					deq.add(new Node(i, j));
 				}
 			}
 		}
-		//탐색
 		bfs();
-		//확인
-		check();
-		
+		System.out.println(check());
 	}
-	//bfs를 통해서 익은 토마토를 탐색합니다.
-	static void bfs() {
-		while(!q.isEmpty()) {
-			int[] pos = q.poll();
-			for(int i = 0; i<4;i++) {
-				int y = pos[0]+dy[i];
-				int x = pos[1]+dx[i];
-				if(x>=0&&x<M&&y>=0&&y<N&&box[y][x]==0) {
-					box[y][x]=box[pos[0]][pos[1]]+1;
-					day = box[y][x];
-					q.add(new int[] {y,x});
-				}
-			}
-		}
-	}
-	//안익은 토마토 확인 및 날짜 출력
-	static void check() {
-		for(int i=0;i<N;i++) {
+	static int check() {
+		for(int i =0; i<N;i++) {
 			for(int j=0;j<M;j++) {
-				if(box[i][j]==0) {
-					System.out.println(-1);
-					return;
+				if(map[i][j]==0) {
+					return -1;
 				}
+				cnt = Math.max(map[i][j],cnt);
 			}
 		}
-		if(day==0) {
-			System.out.println(0);
-		}else {
-			System.out.println(day-1);
+		return cnt-1;
+	}
+	static void bfs() {
+		while (!deq.isEmpty()) {
+			Node node = deq.removeFirst();
+			for (int i = 0; i < 4; i++) {
+				int ny = node.y + dy[i];
+				int nx = node.x + dx[i];
+				if(ny>N-1||ny<0||nx>M-1||nx<0)continue;
+				if(map[ny][nx]!=0)continue;
+				map[ny][nx] = map[node.y][node.x]+1;
+				deq.add(new Node(ny,nx));
+			}
+		}
+	}
+
+	static class Node {
+		int y;
+		int x;
+
+		public Node(int y, int x) {
+			this.y = y;
+			this.x = x;
 		}
 	}
 }
