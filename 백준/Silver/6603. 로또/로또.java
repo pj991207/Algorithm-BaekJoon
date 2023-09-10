fa80d7fcc3977245f4ee7cf1f3894ab82bac1672
ISO-pj991207 <pj991207@gmail.com>
2023-09-10 18:55:55 +0900

    [Silver II] Title: 로또, Time: 332 ms, Memory: 16252 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,48 +1,38 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
 	static int N;
 	static int[] nums;
-	static int[] ans;
-	static StringBuilder sb = new StringBuilder();
-	
+	static int[] answer;
+	static int R = 6;
 	public static void main(String[] args) throws Exception {
-		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
 		while(true) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			N = Integer.parseInt(st.nextToken());
-			
-			if(N==0) break;
-			
-			ans = new int[6];
+			if(N==0) {
+				return;
+			}
 			nums = new int[N];
-			
-			for(int i = 0; i<N;i++) {
+			for(int i= 0; i<N;i++) {
 				nums[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			dfs(0,0);
-			
-			sb.append("\n");
+			answer = new int[R];
+			Combi(0,0);
+			System.out.println("");
 		}
-		
-		System.out.println(sb);
-	}
-	static void dfs(int index, int start) {
-		if(index == 6) {
-			for(int i : ans) {
-				sb.append(i).append(" ");
+	}	
+	static void Combi(int cnt,int start) {
+		if(cnt==R) {
+			for(int e: answer) {
+				System.out.print(e+" ");
 			}
-			sb.append("\n");
+			System.out.println("");
 			return;
 		}
-		for(int i = start; i<N;i++) {
-			ans[index] = nums[i];
-			dfs(index+1,i+1);
+		for(int i = start;i<N;i++) {
+			answer[cnt] = nums[i];
+			Combi(cnt+1,i+1);
 		}
-		
 	}
 }
